43140dc80c1af8958c4bf43ab49174449dc04411
Kirill B <re1f@ya.ru>
2020-04-01 18:50:31 +0500

    Добавьте файлы проекта.

---
+++ 001_Classes/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _001_Classes
+{
+    class Address
+    {
+        private int index;
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+
+        public int Index
+        {
+            set
+            {
+                index = value;
+            }
+            get
+            {
+                return index;
+            }
+        }
+    }
+}
---
+++ 001_Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _001_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address addr1 = new Address
+            {
+                Index = 293057,
+                Country = "USA",
+                City = "Los Santos",
+                Street = "Groove",
+                House = "13",
+                Apartment = "1"
+            };
+
+            Console.WriteLine(addr1.Index);
+            Console.WriteLine(addr1.Country);
+            Console.WriteLine(addr1.City);
+            Console.WriteLine(addr1.Street);
+            Console.WriteLine(addr1.House);
+            Console.WriteLine(addr1.Apartment);
+        }
+    }
+}
